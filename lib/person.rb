973445c9eb5d034e122ab61e470c94dd3a4fe70d
Snicewicz <snicewicz@gmail.com>
2022-01-20 09:21:51 -0500

    commit tree 81eaeedc3ba029d8772a185e1e3eccb39ac45f85

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+
+    def name=(full_name)
+        @name = full_name
+    end
+    
+    def name
+        @name
+    end
+
+    def job=(new_job)
+        @job = new_job
+    end
+
+    def job
+        @job
+    end
+
+
+end
